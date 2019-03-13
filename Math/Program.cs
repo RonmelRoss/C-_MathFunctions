163c6a04c7d7189abb931b42a5acebee83487c72
Ronmel <ronmelross@gmail.com>
2019-03-13 16:15:51 +0800

    002 - Factorial, Nullable types & experimentation on Method parameters

--- Math/Program.cs
+++ Math/Program.cs
@@ -26,18 +26,56 @@ namespace MathFunctions
         public static void Main(string[] args)
         {
             double num = 64354.00;
+            int fibonacciSeq = 10;
+            int checkPrime = 5;
+            int printPrime = 4;
+            int factor = 6;
+            double radius = 10;
+
+            //NumSwap variables
+            int a, b, c, d;
+            a = c = 5;
+            b = d = 10;
 
             Console.WriteLine("Default format: " + num);
 
-            Console.WriteLine("Value with two decimal places: " + "{0:#.##}", num);
+            Console.WriteLine("Value with two decimal places: {0:#.##}", num.ToString());
 
-            Console.WriteLine(Fibonacci(10));
+            Console.WriteLine(Fibonacci(fibonacciSeq));
 
-            Console.WriteLine(IsPrime(1));
+            Console.WriteLine("Is input value prime: "+IsPrime(checkPrime));
 
-            Console.WriteLine(PrimePrint(7));
+            Console.WriteLine("Print prime values: "+PrimePrint(printPrime));
 
-            Console.ReadLine();
+            Console.WriteLine("Area of Circle = "+Shapes.Circle(radius));
+
+            Console.WriteLine("Factorial of " + factor + ": " + Factorial(factor));
+
+            //NumSwap using value parameters
+            Console.WriteLine("\nInitial A = " + a + ", Initial B = " + b);
+            NumSwapValueParam(a, b);
+            Console.WriteLine("Modified A = " + a + ", Modified B = " + b);
+
+            //NumSwap using reference parameters
+            Console.WriteLine("\nInitial C = " + c + ", Initial D = " + d);
+            NumSwapRefParam(ref c, ref d);
+            Console.WriteLine("Modified C = " + c + ", Modified D = " + d);
+
+            //NumSwap using output parameters
+            int x = 3;
+            int y = 2048;
+            Console.WriteLine("\nOld Value: " + x);
+            NumSwapOutParam(y, out x);
+            Console.WriteLine("New Value: " + x);
+
+            //Nullable types
+            int? z1 = null; //Declaration and initialization of nullable type
+            Console.WriteLine("\n"+CheckNullInt(z1));
+
+            string z2 = null;
+            Console.WriteLine(CheckNullString(z2));
+
+            Console.ReadKey();
         }
 
         public static bool IsPrime(int candidate)
@@ -84,22 +122,86 @@ namespace MathFunctions
         }
         public static string PrimePrint(int num)
         {
-            string strPrime = "1 ";
+            string strPrime = "";
 
             int i = 1;
             int j = 1;
-            while (j != num)
+            while (j <= num)
             {
-                if (IsPrime(i))
+                if (j == num)
                 {
-                    strPrime += Convert.ToString(i) + " ";
+                    strPrime += "skip";
                     j++;
                 }
-                //strPrime += Convert.ToString(i)+" ";
+                else if (IsPrime(i))
+                {
+                    strPrime += "skip, ";
+                    j++;
+                } else
+                {
+                    strPrime += Convert.ToString(i) + ", ";
+                }
+                
                 i++;
             }
             return strPrime;
 
+        }
+        public static int Factorial(int factor)
+        {
+            int result;
+            if (factor == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                //Recursion shown below
+                result = Factorial(factor - 1) * factor;
+                return result;
+            }
+        }
+        public static void NumSwapValueParam(int x, int y)
+        {
+            int temp;
+
+            temp = x; /* save the value of x */
+            x = y;    /* put y into x */
+            y = temp; /* put temp into y */
+        }
+        public static void NumSwapRefParam(ref int x, ref int y)
+        {
+            int temp;
+
+            temp = x; /* save the value of x */
+            x = y;    /* put y into x */
+            y = temp; /* put temp into y */
+        }
+        public static void NumSwapOutParam(int y, out int x)
+        {
+            x = y;
+        }
+        public static int CheckNullInt(int? z)
+        {
+            //Checks if int is null or not. If it is, assign value 2048.
+            int val = z ?? 2048;
+            return val;
+        }
+        public static string CheckNullString(string z)
+        {
+            //Checks if string is null or not.
+            string val = z ?? "String is null";
+            return val;
+        }
+    }
+    class Shapes
+    {
+        private const double PI = Math.PI; //Initializing constant value
+
+        public static double Circle(double r)
+        {
+            double area = PI * Math.Pow(r,2);
+            return area;
         }
     }
 }
